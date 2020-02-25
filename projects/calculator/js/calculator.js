3e06eb42e2693a8095cb0e2a176bc96bef836f10
Quenton Max <maxqu01@luther.edu>
2020-02-24 18:04:41 -0800

    add calculator

--- projects/calculator/js/calculator.js
+++ projects/calculator/js/calculator.js
@@ -3,3 +3,31 @@
 /* jshint browser: true */
 /* jshint jquery: true */
 'use strict';
+
+jQuery("screen").fitText(0.38);
+
+function addNewValue(val) 
+{ 
+    if (val == "clear") {
+        document.getElementById("screen").value= " " ;
+
+    }
+    else {
+        document.getElementById("screen").value+=val ;
+    }
+}
+
+function solve() 
+{ 
+    let x = document.getElementById("screen").value ;
+    try{
+        let y = eval(x);
+        document.getElementById("screen").value = y;
+    }
+    catch{
+        document.getElementById("screen").value = "Invalid Calculation. Clearing...";
+        setTimeout(function() { document.getElementById("screen").value= " ";},1000);
+    }
+    
+}
+  
