99e84d015f55a56f9383637575937cb8932969a2
Quenton Max <maxqu01@luther.edu>
2020-04-07 13:52:03 -0700

    final jokes exercise

--- exercises/jokes/app.py
+++ exercises/jokes/app.py
@@ -4,11 +4,27 @@ import pyjokes
 from flask import Flask, request, render_template, url_for, make_response
 
 app = Flask(__name__)
-
-
-@app.route("/", methods=["GET", "POST"])
+#  methods=["GET", "POST"]
+@app.route("/")
 def index():
-    pass
+    category = ["neutral", "chuck", "all"]
+    language = ["en", "de", "es"]
+    return render_template("base.html", category = category, language = language) 
 
-def send_joke(category: str, language: str, number: int):
-    pass
+@app.route("/templates/jokes.html", methods=["GET", "POST"])
+
+def send_joke():
+    category = ["neutral", "chuck", "all"]
+    language = ["en", "de", "es"]
+    cat = request.form['cat']
+    lang = request.form['lang']
+    joke = getJoke(cat,lang)
+    return render_template("jokes.html",JOKE=joke, category = category, language = language)
+    
+def getJoke(cat,lang):
+    ranNum = random.random()
+    jokeList = pyjokes.get_jokes(category=cat, language=lang)
+    ranNum = int(ranNum*len(jokeList))
+    joke = jokeList[ranNum]
+    return joke
+
