e4e31d5023eee4933c686f1a7bd58504a2e30149
Quenton Max <maxqu01@luther.edu>
2020-04-14 12:36:55 -0700

    jokes_api

--- projects/jokes_api/client/script.js
+++ projects/jokes_api/client/script.js
@@ -4,3 +4,17 @@
 /* jshint jquery: true */
 'use strict';
 
+const BASE_URL = "http://localhost:5000/api/v1"
+
+async function requestData(typeOfData) {
+    return fetch(`${BASE_URL}/${typeOfData}`)
+    .then(console.log(typeOfData))
+    .then(response => response.json())
+    .then(json => printData(json[typeOfData]))
+    .catch(error => console.log(error))
+}
+
+function printData(data) {
+    let responseDiv = document.querySelector("#response");
+    responseDiv.innerHTML = data;
+}
--- projects/jokes_api/server/app.py
+++ projects/jokes_api/server/app.py
@@ -2,3 +2,36 @@
 """
 jokes api
 """
+import random
+import pyjokes
+import json
+from flask import Flask, Response, jsonify
+from flask_cors import CORS, cross_origin
+
+
+app = Flask(__name__)
+# CORS(app)
+@app.route("/api/v1/jokes")
+def get_random_joke():
+    ranNum = random.random()
+    jokeList = pyjokes.get_jokes(category="all", language="en")
+    ranNum = int(ranNum*len(jokeList))
+    joke = jokeList[ranNum]
+
+    res = Response(json.dumps({"jokes": joke}))
+    res.headers["Access-Control-Allow-Origin"] = "*"
+    res.headers["Content-Type"] = "application/json"
+    return res
+
+@app.route("/api/v1/jokes/<id>")
+def get_specific_joke(id):
+    
+    jokeList = pyjokes.get_jokes(category="all", language="en")
+    joke = jokeList[int(id)]
+    res = Response(json.dumps({id: joke}))
+    res.headers["Access-Control-Allow-Origin"] = "*"
+    res.headers["Content-Type"] = "application/json"
+    return res
+
+if __name__ == "__main__":
+    app.run("0.0.0.0")
