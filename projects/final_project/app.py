99a3c0c559000e00e1d3e947865e5830c5ed6d5b
Quenton Max <maxqu01@luther.edu>
2020-05-10 19:51:17 -0700

    day 3

--- projects/final_project/app.py
+++ projects/final_project/app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, render_template, send_from_directory
 from flask import redirect, url_for
 import csv, sqlite3
 import sys
+import yfinance as yf
 
 
 app = Flask(__name__)
@@ -71,5 +72,75 @@ def getStockInfo(symbol):
     exString = "SELECT company, sector FROM stockTable WHERE symbol ='"+symbol+"'"
     for row in s.execute(exString):
         company = row 
-    
-    return render_template("stock.html", company=company[0], symbol=symbol, sector=company[1])
+    yourStock = yf.Ticker(symbol)
+    try:
+        stockData = yourStock.info
+        # return stockData
+        
+        logoURL = stockData.get("logo_url")
+
+        address3 = stockData.get("country")
+        if address3 == "United States":
+                address1 = stockData.get("address1")
+                address2 = stockData.get("city")+", "+stockData.get("state")+", "+stockData.get("zip")
+        else:
+            address1=stockData.get("address1")
+            address2=stockData.get("city")+", "+stockData.get("zip")
+        website = stockData.get("website")
+        phone = stockData.get("phone")
+        summ = stockData.get("longBusinessSummary")
+        try:
+            summ = summ.split('.')
+            summ = summ[0]+'.'+summ[1]+'.'+summ[2]+'.'+summ[3]+'.'+summ[4]+'.'+summ[5]+'.'
+        except:
+            pass
+
+        lastPrice = 'Current Price: $'+str(round(((float(stockData.get("bid"))+float(stockData.get("ask")))/2),2))
+        try:
+            div = 'Dividend Yield: '+str(round((stockData.get("dividendYield")*100),2))+'%'
+        except:
+            div = "Dividend Yield: 0.00%"
+        h52 = "52 week high: "+str(stockData.get("fiftyTwoWeekHigh"))
+        l52 = "52 week low: "+str(stockData.get("fiftyTwoWeekLow"))
+        vol = "Today's Volume: "+str(place_value(int(stockData.get("volume"))))
+        avol = "Average Volume: "+str(place_value(int(stockData.get("averageVolume"))))
+        c52 = '52 week change: '+str(round((stockData.get("52WeekChange")*100),2))+'%'
+        s52 = 'S&P 500 52 week change: '+str(round((stockData.get("SandP52WeekChange")*100),2))+'%'
+        mcap = "Market Cap: $"+str(place_value(int(stockData.get("marketCap"))))
+
+
+
+
+        
+
+
+
+        error = ""
+        return render_template("stock.html", company=company[0], symbol=symbol, sector=company[1], logoURL=logoURL, address1=address1, \
+            address2=address2, address3=address3, website=website, phone=phone, lastPrice=lastPrice, div=div, summ=summ, error=error, \
+                h52=h52, l52=l52, vol=vol, avol=avol, c52=c52, s52=s52, mcap=mcap)
+    except:
+        error="No data available for this company."
+        logoURL=""
+        address1=""
+        address2=""
+        address3=""
+        website=""
+        phone=""
+        lastPrice=""
+        div=""
+        summ=""
+        h52 = ""
+        l52 = ""
+        vol = ""
+        avol ="" 
+        c52 = ""
+        s52 = ""
+        mcap ="" 
+
+        return render_template("stock.html", company=company[0], symbol=symbol, sector=company[1], logoURL=logoURL, address1=address1, \
+            address2=address2, address3=address3, website=website, phone=phone, lastPrice=lastPrice, div=div, summ=summ, error=error, \
+                h52=h52, l52=l52, vol=vol, avol=avol, c52=c52, s52=s52, mcap=mcap)
+def place_value(number): 
+    return ("{:,}".format(number)) 
+
